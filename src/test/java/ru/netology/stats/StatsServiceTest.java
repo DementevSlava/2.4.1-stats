33eae839b27ff97c04cd9fbdf08f65a4a4d40af3
Dementev Slava <kursant344@gmail.com>
2020-04-09 13:05:46 +0300

    Create ru.netology.stats

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,6 +1,8 @@
+package ru.netology.stats;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatsServiceTest {
 
