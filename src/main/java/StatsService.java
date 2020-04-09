6acb618c49850166b53febdc247bc54185984773
Dementev Slava <kursant344@gmail.com>
2020-04-09 12:45:22 +0300

    Add test

--- src/main/java/StatsService.java
+++ src/main/java/StatsService.java
@@ -2,20 +2,64 @@ public class StatsService {
 
     public int allSumSales(int[] months) {
         int sum = 0;
-        for (int i=0; i<months.length; i++) {
-            sum += months[i];
+        for (int month : months) {
+            sum += month;
         }
         return sum;
     }
 
     public double middleMonthlySales (int[] months) {
 
-        double sum = (double) allSumSales(months) / months.length;
-        return sum;
+        return (double) allSumSales(months) / months.length;
     }
 
-    public double maxSumSales (int[] months) {
+    public int maxSumSales (int[] months) {
 
+        int max = 0;
+        int maxMonth = 0;
+        for (int i=0; i<months.length; i++) {
+            if (months[i] >= max) {
+                max = months[i];
+                maxMonth = i+1;
+            }
+        }
+        return maxMonth;
+    }
 
+    public int minSumSales (int[] months) {
+
+        int min = maxSumSales(months);
+        int minMonth = 0;
+        for (int i=0; i<months.length; i++) {
+            if (months[i] <= min) {
+                min = months[i];
+                minMonth = i+1;
+            }
+        }
+        return minMonth;
+    }
+
+    public int numberMonthsLower (int[] months) {
+
+        int number = 0;
+
+        for (int month : months) {
+            if (month < middleMonthlySales(months)) {
+                number++;
+            }
+        }
+        return number;
+    }
+
+    public int numberMonthsHigher (int[] months) {
+
+        int number = 0;
+
+        for (int month : months) {
+            if (month > middleMonthlySales(months)) {
+                number++;
+            }
+        }
+        return number;
     }
 }
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -4,13 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
 
-    void calcAllSum () {
+    int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+    void allSumSales() {
         StatsService service = new StatsService();
-
-        int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int allSumSales = service.allSumSales(months);
-
-
+        int expected = 180;
+        assertEquals(expected, allSumSales);
     }
 
+    @Test
+    void middleMonthlySales() {
+        StatsService service = new StatsService();
+        double middleMonthlySales = service.middleMonthlySales(months);
+        int expected = 15;
+        assertEquals(expected, middleMonthlySales);
+    }
+
+    @Test
+    void maxSumSales() {
+        StatsService service = new StatsService();
+        int maxSumSales = service.maxSumSales(months);
+        int expected = 8;
+        assertEquals(expected, maxSumSales);
+    }
+
+    @Test
+    void minSumSales() {
+        StatsService service = new StatsService();
+        int minSumSales = service.minSumSales(months);
+        int expected = 9;
+        assertEquals(expected, minSumSales);
+    }
+
+    @Test
+    void numberMonthsLower() {
+        StatsService service = new StatsService();
+        int numberMonthsLower = service.numberMonthsLower(months);
+        int expected = 5;
+        assertEquals(expected, numberMonthsLower);
+    }
+
+    @Test
+    void numberMonthsHigher() {
+        StatsService service = new StatsService();
+        int numberMonthsHigher = service.numberMonthsHigher(months);
+        int expected = 5;
+        assertEquals(expected, numberMonthsHigher);
+    }
 }
